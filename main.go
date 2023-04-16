e259ae449fdeaea2cd7d56a38a09d6303d562217
Bassam Maged <bassammaged01@gmail.com>
2023-04-16 18:36:23 +0200

    Import currency and platform module

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 
 	"github.com/bassammaged/gosimpleswap/currency"
+	"github.com/bassammaged/gosimpleswap/platform"
 )
 
 func main() {
-	userData := currency.NewUserData("xxx-xxx-xxxxxx-xxxxxx-xxxxxxx")
+	userData := platform.NewUserData("3xeexxxa-8xx4-4dx3-xxx0-606xxxx281xx4")
 
-	currency, err := currency.GetCurrency("BSV", userData)
+	currency := currency.Currency{}
+	object, err := currency.GetCurrency("BSV", *userData)
+	fmt.Println(object)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(currency)
 }
